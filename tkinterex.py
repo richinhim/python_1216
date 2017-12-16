5cd47d814008b7b015ee43fdb5cd932ca242f27a
richinhim <kwlsy666@naver.com>
2017-12-16 18:15:48 +0900

    GUI TkInter ex2

--- tkinterex.py
+++ tkinterex.py
@@ -6,13 +6,15 @@ root = Tk()
 lbl = Label(root, text ="성명")
 lbl.pack() #pack- 배치관리자
 
+
+lbl.grid(row = 0, column=200)
+
 txt = Entry(root)
 
-txt.pack()
+txt.grid(row = 0, column=1)
 
 btn = Button(root, text="OK")
 
-
-btn.pack()
+btn.grid(row = 1, column=1)
 
 root.mainloop()
---
+++ tkinterex5.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+window = Tk()
+
+w = Label(window, text="색깔변환1", bg ="red", fg="white")
+
+w.place(x=0, y=0)
+
+w = Label(window, text="색깔변환2", bg ="green", fg="black")
+
+w.place(x=20,y=20)
+
+window.mainloop()
+
